7a5a249166be9dbf56fe862d3e45bbd6a99ddb57
Rochelle Kaper <61244351+rochellekaper@users.noreply.github.com>
2024-04-19 13:55:33 -0400

    Add files via upload

--- green_purple_orange/green_purple_orange_lastrun.py
+++ green_purple_orange/green_purple_orange_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2022.2.5),
-    on January 09, 2024, at 10:27
+    on March 27, 2024, at 17:42
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
